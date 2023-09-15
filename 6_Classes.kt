35c6177501c9c9cc4aa6a041bb187734e2bc2565
Muana <lalhming.muana1982@gmail.com>
2023-09-15 13:48:36 +0530

    constructors file added

--- 6_Classes.kt
+++ 6_Classes.kt
@@ -41,3 +41,45 @@ fun main() {
     smartTv.turnOn()
     smartTv.turnOff()
 }
+
+/**
+ * class properties emaw class variables siamdan leh hman dan
+ * 
+ */
+
+ class SmartDevice{
+  val name ="Android Tv"
+  val category = "Entertainment"
+  var statusCode = "Online"
+
+	  fun turnOn(){
+          println("Turning on device in 2 seconds")
+      }
+      
+      fun turnOff(){
+          println("Turning off device in 2 seconds")
+      }
+}
+
+
+fun main() {
+    println("Hello, world!")
+    val smartTv = SmartDevice() 
+    println("THe name of the TV is ${smartTv.name}")
+    smartTv.turnOn()
+    smartTv.turnOff()
+}
+
+/**
+ * In the setter function, if you want a certain variable between
+ * 
+ * 0 and 100 you use the following code
+ * 
+ **/
+
+ var speakerVolume = 2
+    set(value) {
+        if (value in 0..100) {
+            field = value
+        }
+    }
---
+++ 7_Constructor.kt
@@ -0,0 +1,50 @@
+/**
+ * 
+ Default constructor
+ 
+ A default constructor is a constructor without parameters. You can define a default constructor as shown in this code snippet:
+ * 
+ */
+
+class SmartDevice constructor() {
+   // code goes here
+}
+
+/**
+ Kotlin aims to be concise, so you can remove the constructor keyword if there are no annotations or visibility modifiers, which you learn about soon, on the constructor. You can also remove the parentheses if the constructor has no parameters as shown in this code snippet:
+ */
+
+class SmartDevice {
+    // some code here
+}
+
+
+
+/**
+  parameterized constructor
+  
+  example is shown below
+
+ */
+class SmartDevice(val name: String, val category: String) {
+    var deviceStatus = "online"
+
+    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+        deviceStatus = when (statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
+   
+}
+
+
+
+fun main(){
+    val smartTvDevice = SmartDevice("Android phone", "Entertainment",0)
+    println("Device name is: ${smartTvDevice.name}")
+    println("Device category is: ${smartTvDevice.category}")
+    println("Device Status is: ${smartTvDevice.deviceStatus}")
+    
+}
